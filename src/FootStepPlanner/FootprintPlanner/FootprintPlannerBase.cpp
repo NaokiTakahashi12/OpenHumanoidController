3155397784293e6bc398a55e0f5d1439ae644db0
NaokiTakahashi <>
2019-11-04 21:24:57 +0900

    Update FootprintPlannerBase

--- src/FootStepPlanner/FootprintPlanner/FootprintPlannerBase.cpp
+++ src/FootStepPlanner/FootprintPlanner/FootprintPlannerBase.cpp
@@ -21,29 +21,59 @@ namespace FootStepPlanner {
 		}
 
 		template <typename Scalar>
+		FootprintPlannerBase<Scalar> &FootprintPlannerBase<Scalar>::set_goal(const decltype((goal_point)) spatial_point) {
+			if(!spatial_point) {
+				throw std::runtime_error("Can not access spatial_point from FootStepPlanner::FootprintPlanner::FootprintPlannerBase");
+			}
+			if(!goal_point) {
+				goal_point = SpatialPoint::make_ptr();
+			}
+			goal_point->centor = spatial_point->centor;
+			goal_point->centor_eular_angles = spatial_point->centor_eular_angles;
+
+			return *this;
+		}
+
+		template <typename Scalar>
 		FootprintPlannerBase<Scalar> &FootprintPlannerBase<Scalar>::set_goal(const Vector &point, const EulerAngles &eularangles) {
 			if(!goal_point) {
-				goal_point = std::make_unique<Footprint::SpatialPoint<Scalar, dimention_rank>>();
+				goal_point = SpatialPoint::make_ptr();
 			}
 			goal_point->centor = point;
 			goal_point->centor_eular_angles = eularangles;
+
 			return *this;
 		}
 
 		template <typename Scalar>
 		FootprintPlannerBase<Scalar> &FootprintPlannerBase<Scalar>::set_goal(const Scalar &x, const Scalar &y, const Scalar &z, const Scalar &a, const Scalar &b, const Scalar &g) {
 			if(!goal_point) {
-				goal_point = std::make_unique<Footprint::SpatialPoint<Scalar, dimention_rank>>();
+				goal_point = SpatialPoint::make_ptr();
 			}
 			goal_point->centor << x, y, z;
 			goal_point->centor_eular_angles << a, b, g;
+
+			return *this;
+		}
+
+		template <typename Scalar>
+		FootprintPlannerBase<Scalar> &FootprintPlannerBase<Scalar>::set_begin(const decltype((begin_point)) spatial_point) {
+			if(!spatial_point) {
+				throw std::runtime_error("Can not access spatial_point from FootStepPlanner::FootStepPlanner::FootprintPlannerBase");
+			}
+			if(!begin_point) {
+				begin_point = SpatialPoint::make_ptr();
+			}
+			begin_point->centor = spatial_point->centor;
+			begin_point->centor_eular_angles = spatial_point->centor_eular_angles;
+
 			return *this;
 		}
 
 		template <typename Scalar>
 		FootprintPlannerBase<Scalar> &FootprintPlannerBase<Scalar>::set_begin(const Vector &point, const EulerAngles &eularangles) {
 			if(!begin_point) {
-				begin_point = std::make_unique<Footprint::SpatialPoint<Scalar, dimention_rank>>();
+				begin_point = SpatialPoint::make_ptr();
 			}
 			begin_point->centor = point;
 			begin_point->centor_eular_angles = eularangles;
@@ -54,11 +84,22 @@ namespace FootStepPlanner {
 		template <typename Scalar>
 		FootprintPlannerBase<Scalar> &FootprintPlannerBase<Scalar>::set_begin(const Scalar &x, const Scalar &y, const Scalar &z, const Scalar &a, const Scalar &b, const Scalar &g) {
 			if(!begin_point) {
-				begin_point = std::make_unique<Footprint::SpatialPoint<Scalar, dimention_rank>>();
+				begin_point = SpatialPoint::make_ptr();
 			}
 			begin_point->centor << x, y, z;
 			begin_point->centor_eular_angles << a, b, g;
+
 			return *this;
+		}
+
+		template <typename Scalar>
+		void FootprintPlannerBase<Scalar>::config(const std::string &new_config_file_name) {
+			config_manager = ConfigManager::make_ptr(new_config_file_name);
+		}
+
+		template <typename Scalar>
+		void FootprintPlannerBase<Scalar>::clear_footprint() {
+			throw std::runtime_error("Unoverride from FootStepPlanner::FootprintPlanner::FootprintPlannerBase");
 		}
 
 		template class FootprintPlannerBase<float>;
--- src/FootStepPlanner/FootprintPlanner/FootprintPlannerBase.hpp
+++ src/FootStepPlanner/FootprintPlanner/FootprintPlannerBase.hpp
@@ -8,9 +8,8 @@
 
 #pragma once 
 
-#include <memory>
-
 #include "../Footprint/SpatialPoint.hpp"
+#include "../ConfigManager.hpp"
 
 namespace FootStepPlanner {
 	namespace FootprintPlanner {
@@ -19,24 +18,33 @@ namespace FootStepPlanner {
 			protected :
 				static constexpr auto dimention_rank = 3;
 
-				using SpatialPointPtr = typename Footprint::SpatialPoint<Scalar, dimention_rank>::Ptr;
+				using SpatialPoint = Footprint::SpatialPoint<Scalar, dimention_rank>;
+				using SpatialPointPtr = typename SpatialPoint::Ptr;
+
+				SpatialPointPtr begin_point, goal_point;
+
+				ConfigManager::Ptr config_manager;
 
 			public :
-				using Vector = Tools::Math::Vector<Scalar, dimention_rank>;
-				using EulerAngles = Vector;
+				using Vector = typename SpatialPoint::Vector;
+				using EulerAngles = typename SpatialPoint::EulerAngles;
 
 				using Ptr = std::unique_ptr<FootprintPlannerBase>;
 
 				FootprintPlannerBase();
 				virtual ~FootprintPlannerBase();
 
+				FootprintPlannerBase &set_goal(const decltype((goal_point)));
 				FootprintPlannerBase &set_goal(const Vector &, const EulerAngles &);
 				FootprintPlannerBase &set_goal(const Scalar &x, const Scalar &y, const Scalar &z, const Scalar &a, const Scalar &b, const Scalar &g);
+				FootprintPlannerBase &set_begin(const decltype((begin_point)));
 				FootprintPlannerBase &set_begin(const Vector &, const EulerAngles &);
 				FootprintPlannerBase &set_begin(const Scalar &x, const Scalar &y, const Scalar &z, const Scalar &a, const Scalar &b, const Scalar &g);
 
-			protected :
-				SpatialPointPtr begin_point, goal_point;
+				virtual void config(const std::string &new_config_file_name);
+
+				virtual void clear_footprint();
+
 		};
 	}
 }
