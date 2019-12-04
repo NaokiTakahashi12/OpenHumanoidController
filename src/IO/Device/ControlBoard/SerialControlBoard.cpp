b208c51b8d3a54194a8fc1225602255fcec388f2
NaokiTakahashi <>
2019-12-04 10:47:53 +0900

    Update serial control board device class

--- src/IO/Device/ControlBoard/SerialControlBoard.cpp
+++ src/IO/Device/ControlBoard/SerialControlBoard.cpp
@@ -8,40 +8,43 @@
 
 #include "SerialControlBoard.hpp"
 
-#include "../../Communicator/SerialController/Dynamixel.hpp"
-
 namespace IO {
 	namespace Device {
 		namespace ControlBoard {
-			template <class CONTROLLER>
-			SerialControlBoard<CONTROLLER>::SerialControlBoard(RobotStatus::InformationPtr &robot_status_information_ptr) : SerialControlBoardBase(robot_status_information_ptr) {
+			SerialControlBoard::SerialControlBoard(RobotStatus::InformationPtr &robot_status_information_ptr) {
+				this->robo_info = robot_status_information_ptr;
 			}
 
-			template <class CONTROLLER>
-			SerialControlBoard<CONTROLLER>::SerialControlBoard(const ID &new_id, RobotStatus::InformationPtr &robot_status_information_ptr) : SerialControlBoardBase(new_id, robot_status_information_ptr) {
+			SerialControlBoard::SerialControlBoard(const ID &new_id, RobotStatus::InformationPtr &robot_status_information_ptr) : SerialControlBoard(robot_status_information_ptr) {
+				id(new_id);
 			}
 
-			template <class CONTROLLER>
-			void SerialControlBoard<CONTROLLER>::register_controller(CommandControllerPtr &command_controller) {
-				if(!this->command_controller) {
-					this->command_controller = command_controller;
-				}
-				else {
-					throw std::runtime_error("Can not regist command controller from IO::Device::ControlBoard::SerialControlBoard");
-				}
+			SerialControlBoard::~SerialControlBoard() {
 			}
 
-			template <class CONTROLLER>
-			void SerialControlBoard<CONTROLLER>::ping() {
+			void SerialControlBoard::ping() {
 				throw std::logic_error("Unoverride from IO::Device::ControlBoard::SerialControlBoard");
 			}
 
-			template <class CONTROLLER>
-			void SerialControlBoard<CONTROLLER>::enable_power(const bool &) {
+			void SerialControlBoard::enable_power(const bool &) {
 				throw std::logic_error("Unoverride from IO::Device::ControlBoard::SerialControlBoard");
 			}
 
-			template class SerialControlBoard<Communicator::SerialController::Dynamixel>;
+			SerialControlBoard::ID &SerialControlBoard::id() const {
+				if(!device_id) {
+					throw std::runtime_error("Failed access to device_id from IO::Device::ControlBoard::SerialControlBoard");
+				}
+
+				return *device_id;
+			}
+
+			void SerialControlBoard::id(const ID &new_id) {
+				if(!device_id) {
+					device_id = std::make_unique<ID>();
+				}
+
+				*device_id = new_id;
+			}
 		}
 	}
 }
