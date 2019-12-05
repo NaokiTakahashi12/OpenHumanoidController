9dec46bce24f064c43e53fc9ca5846a9a2cdf762
mirai <yasuo.hayashibara@it-chiba.ac.jp>
2019-12-05 18:57:25 +0900

    add GankenKunIK

---
+++ src/Kinematics/InverseProblemSolvers/GankenKunIK.cpp
@@ -0,0 +1,101 @@
+
+/**
+  *
+  * @file GankenKunIK.hpp
+  * @author Yasuo Hayashibara
+  *
+  **/
+
+#include "GankenKunIK.hpp"
+
+#include <vector>
+#include <iostream>
+
+#include <rbdl/Kinematics.h>
+
+using namespace GankenKun2018;
+
+namespace Kinematics {
+	namespace InverseProblemSolvers {
+		template <typename Scalar>
+		GankenKunIK<Scalar>::GankenKunIK(ModelPtr &new_model) : MultipleIK<Scalar>(new_model) {
+			link = new Link[Const::LINK_NUM];
+			kine = new GankenKun2018::Kinematics(link);
+			initLink(link);
+			for(int i = 0; i < Const::SERVO_NUM; i ++) servo_angle[i] = 0;
+		}
+
+		template <typename Scalar>
+		GankenKunIK<Scalar>::~GankenKunIK() {
+			delete(kine);
+			delete(link);
+		}
+
+		template <typename Scalar>
+		const std::string GankenKunIK<Scalar>::get_key() {
+			return "GankenKunIK";
+		}
+
+		template <typename Scalar>
+		typename GankenKunIK<Scalar>::Ptr GankenKunIK<Scalar>::make_ptr(ModelPtr &new_model) {
+			return std::make_unique<GankenKunIK>(new_model);
+		}
+
+		template <>
+		bool GankenKunIK<double>::compute() {
+			Link RFLink, LFLink;
+			using Scalar = double;
+			std::vector<ControlPointMap<Scalar>::BodyID> body_ids;
+			for(auto &[body_id, spatial_point] : this->control_point_map->access_to_this_storage()) {
+				if (body_id == Const::ANKLE_ROLL_R + 1) {
+					auto pos = spatial_point.point();
+					RFLink.p << pos[0], pos[1], pos[2];
+				}
+				if (body_id == Const::ANKLE_ROLL_L + 1) {
+					auto pos = spatial_point.point();
+					LFLink.p << pos[0], pos[1], pos[2];
+				}
+			}
+
+			printf("\r\ncompute inverse kinematics\r\n");
+			for(int i = 0; i < Const::SERVO_NUM; i ++) {
+				printf("%f ", servo_angle[i]);
+			}
+			kine->setJointAngle(servo_angle);
+			kine->calcForwardKinematics();
+			kine->calcInverseKinematics(servo_angle, RFLink, LFLink);
+
+			std::cout << RFLink.p << std::endl;
+			std::cout << LFLink.p << std::endl;
+			printf("\r\n");
+			for(int i = 0; i < Const::SERVO_NUM; i ++) {
+				parameters->joint_angle()()(i) = servo_angle[i];
+				printf("%f ", servo_angle[i]);
+			}
+			printf("\r\n");
+
+			return true;
+		}
+
+		template <>
+		[[deprecated("Please use double template.")]]
+		bool GankenKunIK<float>::compute() {
+			return true;
+		}
+		
+		template <>
+		bool GankenKunIK<double>::compute(const Quantity::JointAngle<double> &init_q) {
+			return true;
+		}
+
+		template <>
+		[[deprecated("Please use double template.")]]
+		bool GankenKunIK<float>::compute(const Quantity::JointAngle<float> &init_q) {
+			return true;
+		}
+
+		template class GankenKunIK<float>;
+		template class GankenKunIK<double>;
+	}
+}
+
