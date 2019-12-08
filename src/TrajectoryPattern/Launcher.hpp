e5e08aff4025324cd63ec4380fb250f03e8cc193
NaokiTakahashi <s16c1077hq@s.chibakoudai.jp>
2019-12-08 23:08:51 +0900

    Added launcher for trajectory pattern

---
+++ src/TrajectoryPattern/Launcher.hpp
@@ -0,0 +1,55 @@
+
+/**
+  *
+  * @file Launcher.hpp
+  * @author Naoki Takahashi
+  *
+  **/
+
+#pragma once
+
+#include <atomic>
+
+#include <RobotStatus/Information.hpp>
+#include <Tools/Math/Matrix.hpp>
+
+#include "LinerInvertedPendulum/WalkFragments.hpp"
+
+namespace TrajectoryPattern {
+	class Launcher {
+		public :
+			using Footprints = Tools::Math::MatrixX<LinerInvertedPendulum::WalkFragments::MatrixElement>;
+
+			Launcher(RobotStatus::InformationPtr &);
+			~Launcher();
+
+			void set_config_file(const std::string &dir, const std::string &filename);
+
+			bool is_computing_now() const;
+			void wait_for_computing() const;
+
+			void operator ()();
+
+		private :
+			std::string config_dir,
+						config_filename;
+
+			std::atomic<bool> computing_now;
+
+			float com_hight;
+			float plane_contact_time;
+			float minimize_a,
+				  minimize_b;
+						
+			std::unique_ptr<std::thread> async_update_com_trajectory_thread;
+
+			std::unique_ptr<LinerInvertedPendulum::WalkFragments> liner_inverted_pendulum;
+
+			std::unique_ptr<Footprints> footprints;
+
+			RobotStatus::InformationPtr robo_info;
+
+			void initialize();
+	};
+}
+
